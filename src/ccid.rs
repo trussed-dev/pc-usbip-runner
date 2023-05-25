91dd17cf231cf746a26a12ef296b34bd76b48cc5
Sosthène Guédon <sosthene@nitrokey.com>
2023-05-25 17:48:11 +0200

    Fix compilation

--- src/ccid.rs
+++ src/ccid.rs
@@ -1,11 +1,12 @@
 use apdu_dispatch::dispatch::ApduDispatch;
+use apdu_dispatch::interchanges::Data;
 use usb_device::bus::{UsbBus, UsbBusAllocator};
 use usbd_ccid::Ccid;
 
 pub fn setup<'bus, 'pipe, B: UsbBus>(
     bus_allocator: &'bus UsbBusAllocator<B>,
-    contact: &'pipe apdu_dispatch::interchanges::Channel,
-    contactless: &'pipe apdu_dispatch::interchanges::Channel,
+    contact: &'pipe interchange::Channel<Data, Data>,
+    contactless: &'pipe interchange::Channel<Data, Data>,
 ) -> (Ccid<'bus, 'pipe, B, 3072>, ApduDispatch<'pipe>) {
     let (ccid_rq, ccid_rp) = contact.split().unwrap();
     let ccid = Ccid::new(bus_allocator, ccid_rq, None);
